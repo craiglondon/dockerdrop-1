47bee701df40687333760d28a14d299098c44c37
Lisa Ridley <lisa@savaslabs.com>
2016-12-03 17:45:55 -0500

    Initializing drupal 8

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -eo pipefail
 
-if [ -n "$PHP_SENDMAIL_PATH" ]; then
-     sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /usr/local/etc/php/conf.d/php.ini
-fi
-
 if [ -n "$PHP_MEMORY_LIMIT" ]; then
      sed -i 's@^memory_limit.*@'"memory_limit = ${PHP_MEMORY_LIMIT}"'@' /usr/local/etc/php/conf.d/php.ini
+fi
+
+if [ -n "$PHP_MAX_EXECUTION_TIME" ]; then
+     sed -i 's@^;max_execution_time.*@'"max_execution_time = ${PHP_MAX_EXECUTION_TIME}"'@' /usr/local/etc/php/conf.d/php.ini
 fi
 
 if [ -n "$PHP_POST_MAX_SIZE" ]; then
@@ -15,7 +15,7 @@ if [ -n "$PHP_POST_MAX_SIZE" ]; then
 fi
 
 if [ -n "$PHP_UPLOAD_MAX_FILESIZE" ]; then
-     sed -i 's@^;upload_max_filesize.*@'"post_max_size = ${PHP_UPLOAD_MAX_FILESIZE}"'@' /usr/local/etc/php/conf.d/php.ini
+     sed -i 's@^;upload_max_filesize.*@'"upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}"'@' /usr/local/etc/php/conf.d/php.ini
 fi
 
 exec php-fpm
